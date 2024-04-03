8a3c32968ced5f18c6b7199be411264e82a73064
Vinicius Oliveira <viniciusoliveira.270304@gmail.com>
2024-04-03 11:19:58 -0300

    Initial commit

---
+++ src/main/java/me/dio/sdw2024/application/AskChampionUseCase.java
@@ -0,0 +1,27 @@
+package me.dio.sdw2024.application;
+
+import me.dio.sdw2024.domain.exception.ChampionNotFoundException;
+import me.dio.sdw2024.domain.model.Champion;
+import me.dio.sdw2024.domain.ports.ChampionsRepository;
+import me.dio.sdw2024.domain.ports.GenerativeAiService;
+
+public record AskChampionUseCase(ChampionsRepository repository, GenerativeAiService genAiService) {
+
+    public String askChampion(Long championId, String question)  {
+
+        Champion champion = repository.findById(championId)
+                .orElseThrow(() -> new ChampionNotFoundException(championId));
+
+        String context = champion.generateContextByQuestion(question);
+        String objective = """
+                Atue como assistente com a habilidade de se comportar como os Campeões do League of Legends (LOL).
+                Responda perguntas incorporando a personalidade e estilo de um determinado Campeão.
+                Segue a pergunta, o nome do Campeão e sua respectiva lore (história):
+                
+                """;
+
+        return genAiService.generateContent(objective, context);
+
+
+    }
+}
